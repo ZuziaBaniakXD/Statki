112e098debcc2e51326dec4ba0aab1210a732c6b
Shiperke <105072265+Shiperke@users.noreply.github.com>
2022-06-14 21:16:21 +0200

    Update GameFrame.java

--- src/statki/gui/GameFrame.java
+++ src/statki/gui/GameFrame.java
@@ -1,6 +1,7 @@
 package statki.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GraphicsConfiguration;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
@@ -27,35 +28,96 @@ public class GameFrame extends JFrame implements GameOverListener  {
 	private boolean withComputer;
 	private BoardPanel p1;
 	private BoardPanel p2;
+	String tust,tzak,t1, t2, t3, t4, t5, t6, t7, t8, t9, t11, t12, t13, t14, t15, t16;
+	Sound musicLabel;
+	int M;
 	
-	public GameFrame(boolean withComputer) {
+	
+	
+	public GameFrame(boolean withComputer, int N, Clip audioClip, int SS) {
 		gameMode = SET_SHIPS;
 		this.withComputer = withComputer; //trzyma informacje czy gramy z komputerem (true) czy z innym graczem (false)
 		setSize(800, 600);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setLayout(new BorderLayout());
+		musicLabel = new Sound(1);
+		musicLabel.audioClip = audioClip;
+		musicLabel.n = SS;
+		
+		
+		
+		M=N;
+		
+		if(N % 2 == 1)
+		{
+		tust = "Ustaw";
+		t1 = "Ustaw - Gracz 1";
+		t2 = "Zagraj";
+		t3 = "TWOJA PLANSZA";
+		t4 = "Gracz 1";
+		t5 = "PLANSZA PRZECIWNIKA";
+		t6 = "Gracz 2";
+		t7 = "Zakończ ustawianie - Gracz 1";
+		tzak = "Zakończ";
+		t8= "Ustaw - Gracz 2";
+		t9 = "Zakończ ustawianie - Gracz 2";
+		t11 = "Komputer";
+		t12 = "Gracz nr. 2";
+		t13 = "Gracz";
+		t14 = "Gracz nr. 1";
+		t15 = "Koniec gry, wygrywa ";
+		t16 = "";
+		musicLabel.playbutton.setText("Włącz muzykę");
+		musicLabel.pausebutton.setText("Wyłącz muzykę");
+		}
+		
+		if(N % 2 == 0)
+		{
+		tust = "Set ships";
+		t1 = "Set ships - Player 1";
+		t2 = "Play";
+		t3 = "YOUR BOARD";
+		t4 = "Player 1";
+		t5 = "OPPONENT'S BOARD";
+		t6 = "Player 2";
+		t7 = "Complete setting - Player 1";
+		tzak = "Complete";
+		t8= "Set ships - Player 2";
+		t9 = "Complete setting - Player 2";
+		t11 = "Computer";
+		t12 = "Player 2";
+		t13 = "Player";
+		t14 = "Player 1";
+		t15 = "End of the game, ";
+		t16 = " wins";
+		musicLabel.playbutton.setText("Turn on the music");
+		musicLabel.pausebutton.setText("Turn off the music");
+
+		}
+
 		
 		JPanel upPanel = new JPanel();
-		JButton setShipsButton = new JButton(withComputer ? "Ustaw" : "Ustaw - Gracz 1");
+		JButton setShipsButton = new JButton(withComputer ? tust : t1);
+		
 		upPanel.add(setShipsButton);
-		JButton playButton = new JButton("Zagraj");
+		JButton playButton = new JButton(t2);
 		playButton.setEnabled(false);
 		upPanel.add(playButton);
 		
-		JButton MusicButton1 = new JButton("Włacz muzykę"); 
-		JButton MusicButton2 = new JButton("Wyłącz muzykę");
 		
 		
-		//upPanel.add(MusicButton1);
-		//upPanel.add(MusicButton2);
+		upPanel.add(musicLabel);
+		
+		
 		add(upPanel, BorderLayout.NORTH);
 		
 		JPanel bottomPanel = new JPanel(new GridLayout(1, 2));
 		
-		p1 = new BoardPanel(withComputer ? "TWOJA PLANSZA" : "Gracz 1");
-		p2 = new BoardPanel(withComputer ? "PLANSZA PRZECIWNIKA" : "Gracz 2");
+		p1 = new BoardPanel(withComputer ? t3 : t4);
+		p2 = new BoardPanel(withComputer ? t5 : t6);
 		bottomPanel.add(p1);
 		bottomPanel.add(p2);
+		
 		
 		add(bottomPanel, BorderLayout.CENTER);
 		
@@ -71,22 +133,22 @@ public class GameFrame extends JFrame implements GameOverListener  {
 				else
 				{
 					//jesli gracz nr kliknal w ustaw to daj mu opcje zakonczenia ustawiania
-					if(setShipsButton.getText().contains("1") && setShipsButton.getText().contains("Ustaw"))
+					if(setShipsButton.getText().contains("1") && setShipsButton.getText().contains(tust))
 					{
-						setShipsButton.setText("Zakoncz ustawianie - Gracz 1");
+						setShipsButton.setText(t7);
 						p1.showShips();
 					} //jesli gracz 1 kliknal w zakonczenie ustawiania to nadaj opcje ustawienai stakow dla drugiego gracza
-					else if(setShipsButton.getText().contains("1") && setShipsButton.getText().contains("Zakoncz"))
+					else if(setShipsButton.getText().contains("1") && setShipsButton.getText().contains(t7))
 					{
-						setShipsButton.setText("Ustaw - Gracz 2");
+						setShipsButton.setText(t8);
 						p1.hideShips();
 					}//jesli drugi gracz klikal w opcje ustawienia statkow to daj mu opcje zakonczenia ustawiania
-					else if(setShipsButton.getText().contains("2") && setShipsButton.getText().contains("Ustaw"))
+					else if(setShipsButton.getText().contains("2") && setShipsButton.getText().contains(tust))
 					{
-						setShipsButton.setText("Zakoncz ustawianie - Gracz 2");
+						setShipsButton.setText(t9);
 						p2.showShips();
 					}//jesli drugi gracz zakonczyl ustawianie to aktywuj przycisk rozpoczenia gry
-					else if(setShipsButton.getText().contains("2") && setShipsButton.getText().contains("Zakoncz"))
+					else if(setShipsButton.getText().contains("2") && setShipsButton.getText().contains(tzak))
 					{
 						setShipsButton.setEnabled(false);
 						playButton.setEnabled(true);
@@ -165,24 +227,27 @@ public class GameFrame extends JFrame implements GameOverListener  {
 		String winner = "";
 		if(failedBoard == p1)
 		{
-			winner = (withComputer ? "komputer" : "gracz nr 2");
+			winner = (withComputer ? t11 : t12);
 		}
 		else if(failedBoard == p2)
 		{
-			winner = (withComputer ? "gracz" : "gracz nr 1");
+			winner = (withComputer ? t13 : t14);
 		}
-		JOptionPane.showMessageDialog(this, "Koniec gry, wygrywa " + winner);
-		MenuFrame menu;
-		try {
-			menu = new MenuFrame();
-			menu.setVisible(true);
-		} catch (HeadlessException e) {
+		JOptionPane.showMessageDialog(this, t15 + winner + t16);
+	//	MenuFrame menuFrame;
+	//	try {
+	//		menuFrame = new MenuFrame(M);
+	//		menuFrame.setLocationRelativeTo(null);
+	//		menuFrame.setResizable(false);
+	//		menuFrame.setVisible(true);
+			
+	//	} catch (HeadlessException e) {
+	//		// TODO Auto-generated catch block
+	//		e.printStackTrace();
+	//	} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	//		e.printStackTrace();
+	//	}
 		
 		dispose();
 	}
